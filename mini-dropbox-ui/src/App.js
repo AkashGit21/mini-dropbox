1a77f5bef2816249ae946fd0ea44bceeaa6630fd
Akash Gupta <akashgzb98@gmail.com>
2023-09-05 01:15:52 +0530

    working ui

--- mini-dropbox-ui/src/App.js
+++ mini-dropbox-ui/src/App.js
@@ -1,189 +1,36 @@
-import { Routes, Route} from "react-router-dom";
-// import { useState, useEffect } from "react";
-// import FileView from "./components/FileView";
-import FileView from "./components/FileView";
-import GridView from "./components/GridView";
-
-// const User = () => {
-//   return (
-//     <>
-//       <h2>User</h2>
-
-//       <nav>
-//         <Link to="/user/profile">Profile</Link>
-//         <Link to="/user/account">Account</Link>
-//       </nav>
-//     </>
-//   );
-// };
-
-// const Users = ({ users }) => {
-//   return (
-//     <>
-//       <h2>Users</h2>
-
-//       <ul>
-//         {users.map((user) => (
-//           <li key={user.id}>
-//             <Link to={user.id}>{user.fullName}</Link>
-//           </li>
-//         ))}
-//       </ul>
-
-//       <Outlet />
-//     </>
-//   );
-// };
-
-// const UsersPage = () => {
-//   return (
-//     <>
-//       <h1>Users page</h1>
-
-//       <Outlet />
-//     </>
-//   );
-// };
-
-// const File = () => {
-//   return (
-//     <>
-//       <h2>User</h2>
-
-//       <nav>
-//         <Link to="/user/profile">Profile</Link>
-//         <Link to="/user/account">Account</Link>
-//       </nav>
-//     </>
-//   );
-// };
-
-// const Files = () => {
-//   return (
-//     <>
-//       <h2>Files</h2>
-
-//       <Outlet />
-//     </>
-//   );
-// }
-
-// const FilesPage = () => {
-//   return (
-//     <>
-//       <h1>Files page</h1>
-
-//       <Outlet />
-//     </>
-//   );
-// }
+import { Routes, Route, Link } from "react-router-dom";
+import Files from "./components/Files";
+import FileUpload from "./components/FileUpload";
+import File from "./components/File";
+import FilesPage from "./components/FilesPage";
+import NoMatch from "./components/NoMatch";
+import './App.css';
 
 const App = () => {
-  // const users = [
-  //   { id: "1", fullName: "Robin Wieruch" },
-  //   { id: "2", fullName: "Sarah Finnley" }
-  // ];
-
-  
-
   return (
     <>
-      <h1>React Router</h1>
-    <GridView/>
-      {/* <ul>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/files"><GridView files={items}/></Link>
-        </li>
-      </ul>
-  */}
+      <nav className="navbar">
+        <div className="navbar-brand">Mini Dropbox</div>
+        <ul className="navbar-list">
+          <li className="navbar-item">
+            <Link to="/" className="navbar-item-link">Home</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/files" className="navbar-item-link">Files</Link>
+          </li>
+        </ul>
+      </nav>
+
       <Routes>
-        <Route path="/" element={"Home"} />
-        <Route path="/files" element={<GridView />}>
-          <Route index element={<GridView />} />
-          <Route path=":fileId" element={<FileView />} />
+        <Route path="/" element={<FileUpload />} />
+        <Route path="/files" element={<FilesPage />}>
+          <Route index element={<Files />} />
+          <Route path=":fileId" element={<File />} />
         </Route>
-      </Routes> 
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </>
   );
 };
 
 export default App;
-
-
-
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-// import GridView from './components/GridView';
-// // import Home from './Home'; // Replace with your Home component
-// // import About from './About'; // Replace with your About component
-// // import Contact from './Contact'; // Replace with your Contact component
-// // import NotFound from './NotFound'; // Replace with your Not Found (404) component
-
-// function App() {
-//   return (
-//     <Router>
-//       <div>
-//         {/* Navigation */}
-//         <nav>
-//           <ul>
-//             <li>
-//               <Link to="/">Home</Link>
-//             </li>
-//             {/* <li>
-//               <Link to="/about">About</Link>
-//             </li>
-//             <li>
-//               <Link to="/contact">Contact</Link>
-//             </li> */}
-//           </ul>
-//         </nav>
-
-//         {/* Route Configuration */}
-//         <Routes>
-//           <Route path="/" element={<GridView />} />
-//           {/* <Route path="/about" element={<About />} />
-//           <Route path="/contact" element={<Contact />} />
-//           <Route path="*" element={<NotFound />} /> Handle 404 */}
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
-// import React, { Component } from 'react';
-// import './App.css';
-// // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import GridView from './components/GridView';
-// import FileView from './components/FileView';
-
-// class App extends Component {
-//   render() {
-//       // return (
-//       //   <div className='App'>
-//       //   {/* <Router>
-//       //     <Switch> */}
-//       //       {/* <Route path="/" exact component={GridView} /> */}
-//       //       {/* <Route path="/files/:fileId" component={FileView} />
-//       //     </Switch>
-//       //   </Router> */}
-//       //   </div>
-//       // );
-    
-//       return (
-//       <div className="App">
-        
-//         <GridView/>
-//         <FileView/>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
--- mini-dropbox-ui/src/components/config.js
+++ mini-dropbox-ui/src/components/config.js
@@ -1,4 +1,5 @@
 
-const apiHost = process.env.MINI_DROPBOX_BACKEND_HOST;
+// const apiHost = process.env.MINI_DROPBOX_BACKEND_HOST;
+const apiHost = 'http://localhost:8082';
 
 export {apiHost};
